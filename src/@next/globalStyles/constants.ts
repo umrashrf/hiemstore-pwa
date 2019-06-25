d473a254f0e956fd6065fcd5a3c876783ac6cdf1
jwm0 <majorek.jakub@gmail.com>
2019-06-25 14:03:48 +0200

    Adjust theme according to new guidelines

--- src/@next/globalStyles/constants.ts
+++ src/@next/globalStyles/constants.ts
@@ -14,8 +14,8 @@ export const grayLight = "#f1f5f5";
 export const green = "#3ed256";
 export const overlayColor = "rgba(199, 207, 207, 0.8)";
 export const rose = "#c22d74";
-export const turquoise = "#3ee7cd";
-export const turquoiseDark = "#42beab";
+export const turquoise = "#13bebb";
+export const turquoiseDark = "#06a09e";
 export const white = "#fff";
 
 // theme colors
